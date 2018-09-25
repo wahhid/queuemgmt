b9ebb72e8602512adb964324f6a5704966f4751d
wahhid <wahhid@gmail.com>
2018-09-25 22:10:44 +0700

    Fix Pickup Log

--- jakc_queue/controllers/controllers.py
+++ jakc_queue/controllers/controllers.py
@@ -18,25 +18,6 @@ class QueuePickup(http.Controller):
         type_ids = env_type.sudo().search_read(args)
         return json.dumps(type_ids)
 
-    def _trans_close(self, trans):
-        trans_data = {}
-        trans_data.update({'end_time': datetime.now().strftime('%Y-%m-%d')})
-        trans_data.update({'state': 'done'})
-        trans.sudo().write(trans_data)            
-    
-    @http.route('/queue/pickupsound/', auth='public')
-    def appprint(self, **kw):
-        env_trans_print = http.request.env['queue.trans.sound']
-        transs = env_trans_print.sudo().search([('state','=','open')])
-        if transs:
-            trans = transs[0]
-            trans_data = {}
-            trans_data.update({'state': 'done'})
-            trans.sudo().write(trans_data)            
-            return '{"success":true,"message":"","trans_id":"' + trans.trans_id.trans_id + '"}'
-        else:
-            return '{"success":false,"message":"No Sound Data"}'
-
     @http.route('/queue/pickupui/', auth='public')
     def pickupui(self, **kw):    
         env_pickup = http.request.env['queue.pickup']
@@ -199,14 +180,6 @@ class Queue_display(http.Controller):
             trans.update({'status': False, 'counter_trans': {}})
         return json.dumps(trans)
 
-class Queue(http.Controller):
-    @http.route('/queue/queue/<int:id>', auth='public')
-    def index(self, id, **kw):
-        Types = http.request.env['queue.type']
-        return http.request.render('jakc_queue.index', {
-            'types': Types.search([('id','=', id)])
-        })
-
 
 class Queue_type(http.Controller):
     
@@ -226,18 +199,6 @@ class Queue_type(http.Controller):
 
 
 class Queue_app(http.Controller):
-    
-    def trans_close(self):
-        trans_data = {}
-        trans_data.update({'end_time': datetime.now().strftime('%Y-%m-%d')})
-        trans_data.update({'state':'done'})
-        return True
-        
-    @http.route('/queue/appui/', auth='public')
-    def appui(self, **kw):
-        env_type = http.request.env['queue.type']
-        types = env_type.sudo().search([])                 
-        return http.request.render('jakc_queue.appui', {'types':types})
 
     @http.route('/queue/kiosk/request/<int:type_id>/', auth='public', csrf=False)
     def app(self, type_id, **kw):
@@ -256,13 +217,6 @@ class Queue_app(http.Controller):
             return json.dumps(trans_data)
         except:
             return '{"success":false,"message":"Error"}'
-
-    @http.route('/pos/appprint/receipt/<int:id>', type='http', auth='user')
-    def print_sale_details(self, id, **kw):
-        r = request.env['report.point_of_sale.report_saledetails']
-        pdf = request.env['report'].with_context(id=id).get_pdf(r, 'point_of_sale.report_saledetails')
-        pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', len(pdf))]
-        return request.make_response(pdf, headers=pdfhttpheaders)
 
     @http.route('/queue/print/receipt/<int:id>', auth='public', auth='user')
     def queue_print_receipt(self, id, **kw):
