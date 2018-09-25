3decec923cd5ad893586711df4524585a5390c68
wahhid <wahhid@gmail.com>
2018-09-25 22:45:10 +0700

    Fix Pickup Log

--- jakc_queue/static/src/js/receipt.js
+++ jakc_queue/static/src/js/receipt.js
@@ -1,4 +1,6 @@
 $(document).ready(function (e){
+    console.log("Start Print");
     setTimeout(function () { window.print(); }, 500);
+    console.log("On Focus");
     window.onfocus = function () { setTimeout(function () { window.close(); }, 500); }
 });
