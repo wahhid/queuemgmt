b2f259b8d8423222cf4f080d267b14c3c47854fc
wahhid <wahhid@gmail.com>
2018-09-25 14:56:25 +0700

    Fix Pickup Log

--- jakc_queue/__manifest__.py
+++ jakc_queue/__manifest__.py
@@ -18,8 +18,8 @@
     'data': [
         'security/jakc_queue_security.xml',
         'security/ir.model.access.csv',
-        'views/jakc_queue_menu.xml',
         'views/jakc_queue_view.xml',
+        'views/jakc_queue_menu.xml',
         'views/jakc_queue_pickup_dashboard.xml',
         'views/templates.xml',
         'views/routing_screen_templates.xml',
